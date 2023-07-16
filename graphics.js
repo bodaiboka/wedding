262491165cee57e19a805eb38e9620ec6c7970f8
boka <bodaiboka@gmail.com>
2023-07-16 10:13:34 +0200

    Adding outdoor models

--- graphics.js
+++ graphics.js
@@ -6,7 +6,13 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 var myCanvas = document.getElementById("wedding-canvas");
 let siteWrapper = document.getElementById("site-wrapper");
 let detailsBtn = document.querySelector('.details');
+let civilBtn = document.querySelector('.civil-btn');
+let churchBtn = document.querySelector('.church-btn');
+let dinnerBtn = document.querySelector('.dinner-btn');
+let cakeBtn = document.querySelector('.cake-btn');
+let infoBtn = document.querySelector('.info-btn');
 let churchModel = null;
+let civilModel = null;
 
 var resizeObserver = new ResizeObserver(entry => {
     console.log("canvas change");
@@ -62,6 +68,11 @@ renderer.setClearColor( 0x000000, 0 );
 renderer.setPixelRatio(window.devicePixelRatio);
 camera.position.y = 0;
 camera.position.z = 16;
+let lastcamerapos = {
+    x: 0.0,
+    y: -4.7,
+    z: 16.0
+};
 
 const sc = 0.4; // scale
 const scw = 0.13;
@@ -70,6 +81,7 @@ let dove;
 const clock = new THREE.Clock();
 const loader = new GLTFLoader();
 const loader2 = new GLTFLoader();
+const loader3 = new GLTFLoader();
 loader.load(
 	'models/dove.glb',
 
@@ -101,14 +113,12 @@ loader.load(
 );
 
 loader2.load(
-	'models/churchbake.glb',
+	'models/churchbake2.glb',
 	function ( gltf ) {
 		const model = gltf.scene;
         churchModel = model;
 		console.log(model);
 		scene.add(model);
-		const grass = scene.getObjectByName("GRASS001");
-		console.log(grass);
 		model.scale.set(scw, scw, scw);
         model.translateY(-6.4);
         model.rotateY(32)
@@ -120,6 +130,29 @@ loader2.load(
             gammaRad += Math.PI / 180 * 32.0;
             betaRad -= Math.PI / 180 * 16.0;
             churchModel.rotation.set(betaRad, gammaRad, 0);
+        }, false);
+	}
+);
+
+loader3.load(
+	'models/civil.glb',
+	function ( gltf ) {
+		const model = gltf.scene;
+        civilModel = model;
+		console.log(model);
+		scene.add(model);
+		model.scale.set(0.13, 0.13, 0.13);
+        model.translateY(-6.4);
+        model.translateX(10.4);
+        model.rotateY(32)
+        window.addEventListener('deviceorientation', function(event) {
+            let beta = (event.beta >= 45 ? 45 : (event.beta <= 0 ? 0 : event.beta)) / 2.0; 
+            let gamma = (event.gamma >= 45 ? 45 : (event.gamma <= -45 ? -45 : event.gamma)); // Y-axis rotation in degrees
+            let gammaRad = gamma * (Math.PI / 180);
+            let betaRad = beta * (Math.PI / 180);
+            gammaRad += Math.PI / 180 * 32.0;
+            betaRad -= Math.PI / 180 * 16.0;
+            civilModel.rotation.set(betaRad, gammaRad, 0);
         }, false);
 	}
 );
@@ -139,7 +172,8 @@ animate();
 detailsBtn.addEventListener('click', () => {
     if (!isInfoOpen) {
         const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
-        .to({x: camera.position.x, y: -4.7, z: camera.position.z}, 1000);
+        .to({x: lastcamerapos.x, y: lastcamerapos.y, z: lastcamerapos.z}, 1000);
+        //.to({x: 0, y: -4.7, z: 16}, 1000);
 
         tween1.onUpdate((object, elapsed) => {
             camera.position.y = object.y;
@@ -150,6 +184,9 @@ detailsBtn.addEventListener('click', () => {
             dove.translateY(-6);
         })
     } else {
+        lastcamerapos.x = camera.position.x;
+        lastcamerapos.y = camera.position.y;
+        lastcamerapos.z = camera.position.z;
         const tween2 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
         .to({x: camera.position.x, y: 0, z: camera.position.z}, 1000);
 
@@ -161,6 +198,123 @@ detailsBtn.addEventListener('click', () => {
         dove.translateY(6);
         
     }
-    
 })
+
+churchBtn.addEventListener('click', () => {
+    console.log("civil clicked");
+    const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
+    .to({x: 0, y: -4.7, z: 16}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+
+    const tween2 = new TWEEN.Tween({x: camera.rotation.x, y: camera.rotation.y, z: camera.rotation.z })
+        .to({x: 0, y:0, z: 0}, 500)
+        .easing(TWEEN.Easing.Sinusoidal.Out);;
+
+        tween1.onUpdate((object, elapsed) => {
+            camera.position.x = object.x;
+            camera.position.y = object.y;
+            camera.position.z = object.z;
+        })
+        tween2.onUpdate((object, elapsed) => {
+            camera.rotation.x = object.x;
+            camera.rotation.y = object.y;
+            camera.rotation.z = object.z;
+        })
+        tween1.start();
+        tween2.start();
+})
+
+civilBtn.addEventListener('click', () => {
+    const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
+    .to({x: 11.85, y: -6.8, z: 3}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+    
+    const tween2 = new TWEEN.Tween({x: camera.rotation.x, y: camera.rotation.y, z: camera.rotation.z })
+    .to({x: 0, y: Math.PI / 6.0, z: 0}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+
+        tween1.onUpdate((object, elapsed) => {
+            camera.position.x = object.x;
+            camera.position.y = object.y;
+            camera.position.z = object.z;
+        })
+        tween2.onUpdate((object, elapsed) => {
+            camera.rotation.x = object.x;
+            camera.rotation.y = object.y;
+            camera.rotation.z = object.z;
+        })
+        tween1.start();
+        tween2.start();
+})
+
+dinnerBtn.addEventListener('click', () => {
+    const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
+    .to({x: 15.2, y: -6.6, z: 3}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+    
+    const tween2 = new TWEEN.Tween({x: camera.rotation.x, y: camera.rotation.y, z: camera.rotation.z })
+    .to({x: 0, y: Math.PI / 3.5, z: 0}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+
+        tween1.onUpdate((object, elapsed) => {
+            camera.position.x = object.x;
+            camera.position.y = object.y;
+            camera.position.z = object.z;
+        })
+        tween2.onUpdate((object, elapsed) => {
+            camera.rotation.x = object.x;
+            camera.rotation.y = object.y;
+            camera.rotation.z = object.z;
+        })
+        tween1.start();
+        tween2.start();
+})
+
+cakeBtn.addEventListener('click', () => {
+    const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
+    .to({x: 11, y: -7, z: 3}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+    
+    const tween2 = new TWEEN.Tween({x: camera.rotation.x, y: camera.rotation.y, z: camera.rotation.z })
+    .to({x: 0, y: Math.PI / 6.0, z: 0}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+
+        tween1.onUpdate((object, elapsed) => {
+            camera.position.x = object.x;
+            camera.position.y = object.y;
+            camera.position.z = object.z;
+        })
+        tween2.onUpdate((object, elapsed) => {
+            camera.rotation.x = object.x;
+            camera.rotation.y = object.y;
+            camera.rotation.z = object.z;
+        })
+        tween1.start();
+        tween2.start();
+})
+
+infoBtn.addEventListener('click', () => {
+    const tween1 = new TWEEN.Tween({x: camera.position.x, y: camera.position.y, z: camera.position.z })
+    .to({x: 14.9, y: -5.9, z: 8}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+    
+    const tween2 = new TWEEN.Tween({x: camera.rotation.x, y: camera.rotation.y, z: camera.rotation.z })
+    .to({x: 0, y: Math.PI / 6.0, z: 0}, 500)
+    .easing(TWEEN.Easing.Sinusoidal.Out);
+
+        tween1.onUpdate((object, elapsed) => {
+            camera.position.x = object.x;
+            camera.position.y = object.y;
+            camera.position.z = object.z;
+        })
+        tween2.onUpdate((object, elapsed) => {
+            camera.rotation.x = object.x;
+            camera.rotation.y = object.y;
+            camera.rotation.z = object.z;
+        })
+        tween1.start();
+        tween2.start();
+        
+})
+
 
